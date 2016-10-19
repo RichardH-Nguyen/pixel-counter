7ab418914ca9c7cbf6257e9d49ae580b96628ad0
su7163if <su7163if@T301002-1625318>
2016-10-18 20:21:22 -0500

    Pixel count updates on resize.

--- githubTest/Form1.cs
+++ githubTest/Form1.cs
@@ -30,5 +30,10 @@ namespace githubTest
             int pixels = height * width;
             return pixels;
         }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int pixels = getPixels();
+            pixelsLabel.Text = "After resizing, this window contains " + pixels + " pixels.";
+        }
     }
 }
